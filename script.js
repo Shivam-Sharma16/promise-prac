742537a548bd4f08322978093d6584728d16c968
Shivam-Sharma16 <shivamsharma220380@gmail.com>
2025-06-15 23:14:35 +0530

    task 2 done chained promises

--- script.js
+++ script.js
@@ -1,20 +1,78 @@
 //task 1 food delivery
 
-function orderFood(){
-    return new Promise((res,rej)=>{
-       const val= Math.random()<0.7
-        setTimeout(() => {
+// function orderFood(){
+//     return new Promise((res,rej)=>{
+//        const val= Math.random()<0.7
+//         setTimeout(() => {
 
-            if (val) res();
-            else rej()
-        }, 2000);
-    });
+//             if (val) res();
+//             else rej()
+//         }, 2000);
+//     });
+// }
+
+// orderFood().then(function(){
+//     console.log("🍕 Pizza Delivered!");
+    
+// }).catch(()=>{
+//     console.log("❌ Delivery failed");
+    
+// })
+
+//task 2 chained promises: user->posts->comments
+
+function getUser(){
+return new Promise((res,rej)=>{
+    setTimeout(() => {
+         res({id:1,name:'shivam'})
+    }, 1000);
+   
+})
+
 }
 
-orderFood().then(function(){
-    console.log("🍕 Pizza Delivered!");
-    
-}).catch(()=>{
-    console.log("❌ Delivery failed");
+function getPosts(userId){
+    return new Promise((res,rej)=>{
+        setTimeout(() => {
+            res(['title 1','title 2'])
+        }, 1000);
+        
+    })
+}
+function getComments(postId){
+    return new Promise((res, rej) => {
+        setTimeout(() => {
+            res(['great','inshightful','amazing'])
+        }, 1000);
+        
+    })
+}
+
+
+
+
+getUser().then((data)=>{
+console.log(data);
+ 
+return getPosts(data.id)
+
+})
+.then((title)=>{
+    console.log(title);
+   
+   return getComments('adncs') 
     
 })
+.then((cmts)=>{
+    console.log(cmts);
+    
+})
+.finally(()=>
+{
+    setTimeout(() => {
+        console.log('all steps done');
+    }, 1000);
+    
+    
+})
+
