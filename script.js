fd2a733958a7069b8e833fb1c119289961c5c627
Shivam-Sharma16 <shivamsharma220380@gmail.com>
2025-06-15 23:30:01 +0530

    task 3 done FakeApiCall

--- script.js
+++ script.js
@@ -21,58 +21,84 @@
 
 //task 2 chained promises: user->posts->comments
 
-function getUser(){
-return new Promise((res,rej)=>{
-    setTimeout(() => {
-         res({id:1,name:'shivam'})
-    }, 1000);
+// function getUser(){
+// return new Promise((res,rej)=>{
+//     setTimeout(() => {
+//          res({id:1,name:'shivam'})
+//     }, 1000);
    
-})
+// })
 
-}
+// }
 
-function getPosts(userId){
-    return new Promise((res,rej)=>{
-        setTimeout(() => {
-            res(['title 1','title 2'])
-        }, 1000);
+// function getPosts(userId){
+//     return new Promise((res,rej)=>{
+//         setTimeout(() => {
+//             res(['title 1','title 2'])
+//         }, 1000);
         
-    })
-}
-function getComments(postId){
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            res(['great','inshightful','amazing'])
-        }, 1000);
+//     })
+// }
+// function getComments(postId){
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             res(['great','inshightful','amazing'])
+//         }, 1000);
         
-    })
-}
+//     })
+// }
 
 
 
 
-getUser().then((data)=>{
-console.log(data);
+// getUser().then((data)=>{
+// console.log(data);
  
-return getPosts(data.id)
+// return getPosts(data.id)
 
-})
-.then((title)=>{
-    console.log(title);
+// })
+// .then((title)=>{
+//     console.log(title);
    
-   return getComments('adncs') 
+//    return getComments('adncs') 
     
-})
-.then((cmts)=>{
-    console.log(cmts);
+// })
+// .then((cmts)=>{
+//     console.log(cmts);
     
-})
-.finally(()=>
-{
-    setTimeout(() => {
-        console.log('all steps done');
-    }, 1000);
+// })
+// .finally(()=>
+// {
+//     setTimeout(() => {
+//         console.log('all steps done');
+//     }, 1000);
     
     
-})
+// })
 
+//task 3 : Fake APi Delay
+
+function fakeApiCall(endpoint){
+    const data = {
+        users:['user1', 'user2','user3'],
+        posts:['aaa','adad','fds'],
+
+    };
+     let delay= Math.random()*2000 +1000;
+  return new Promise((res,rej)=>{
+   
+setTimeout(() => {
+ res (data[endpoint])
+}, delay);
+
+   })
+}
+
+fakeApiCall("users").then(function(data){
+console.log(data);
+
+})
+fakeApiCall("posts").then(function(data){
+console.log(data);
+
+})
